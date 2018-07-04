aee5d91e307930a99d0ed3f1e2917594ffafe7df
Andrew <coreand.work@gmail.com>
2018-07-04 12:27:43 +0400

    staging

--- parser.py
+++ parser.py
@@ -2,7 +2,6 @@ import csv
 import datetime
 
 import requests
-import time
 
 
 class Place:
@@ -11,87 +10,131 @@ class Place:
         self.id = id
         self.lat = lat
         self.long = long
-        self.pollution = {}
-        self.weather = {}
+        self.pollution = []
 
 
-def get_pollution(places, unix_time, params):
-    pollution_key = 'x9HE9OTzXpQIRFaHZljhePhOq6egQ8ko2AnGmOq0l6BfWm4w'
+places = [
+    Place('Beijing', 2038349, 39.9042, 116.4074),
+    Place('Zhangjiakou', 2033194, 40.7675, 114.8863),
+    Place('Qinhuangdao', 1797593, 39.9354, 119.6005),
+    Place('Shijiazhuang', 1795266, 38.0428, 114.5149),
+    Place('Tianjin', 1792943, 39.3434, 117.3616),
+]
 
+start_time = datetime.datetime(2017, 3, 31, hour=0)
+end_time = start_time + datetime.timedelta(days=1)
+
+
+# end_time = datetime.datetime.now()
+
+
+def write_csv(name, places, params, data):
+    time_header = 'Timestamp (UTC)'
+
+    header = [time_header]
     for place in places:
-        r = requests.get('https://api.origins-china.cn/v1/places/{}?key={}'.format(place.id, pollution_key))
+        for param in params:
+            header.append('{} {}'.format(place.name, param))
 
-        pollution = r.json()['info.aqi']['data']
+    with open(name + '.csv', 'w') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter='\t')
+        csv_writer.writerow(header)
+        csv_writer.writerows(data)
 
-        # 3 cities don't have o3 field but have aqi.cn instead
+
+class PlaceData:
+    def __init__(self, place, pollution) -> None:
+        self.place = place
+        self.pollution = pollution
+
+
+# r_time.strftime('%Y-%m-%d %H:%M')
+
+
+def get_place_pollution(place, r_start, r_end, params):
+    key = 'x9HE9OTzXpQIRFaHZljhePhOq6egQ8ko2AnGmOq0l6BfWm4w'
+    url = 'https://api.origins-china.cn/v1/places/{}/history?series=raw&begin={}&end={}&key={}'
+
+    r = requests.get(url.format(place.id, r_start, r_end, key))
+    info = r.json()['info.aqi']
+
+    for hour_info in info:
+        pollution = hour_info['data']
+        timestamp = hour_info['ts'].replace('T', ' ').replace(':00Z', '')
+        hour_data = [timestamp]
+
         for param in params:
             try:
                 val = pollution[param]
             except KeyError:
                 val = None
             finally:
-                place.pollution.update({param: val})
+                hour_data.append(val)
+
+        place.pollution.append(hour_data)
 
 
-def get_weather(places, unix_time, params):
-    darksky_key = '4317146d63a0e039a4110e3ea201bd3d'
+def get_pollution():
+    params = ['co', 'no2', 'o3', 'pm10', 'pm25', 'so2']
 
-    for place in places:
-        r = requests.get(
-            'https://api.darksky.net/forecast/{}/{},{},{}'.format(darksky_key, place.lat, place.long, unix_time))
+    delta = datetime.timedelta(days=30)
 
-        weather = r.json()['currently']
+    r_time = start_time
+    while r_time < end_time:
+        r_start = r_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+        r_time += delta
+        r_end = r_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-        # windBearing field may be missing
-        for param in params:
-            try:
-                val = weather[param]
-            except KeyError:
-                val = None
-            finally:
-                place.weather.update({param: val})
+        for place in places:
+            get_place_pollution(place, r_start, r_end, params)
+
+        r_time += delta
+
+    for idx, place in enumerate(places):
+        write_csv(place.name, [place], params, place.pollution)
+
+    data = []
+    first_pollution = places[0].pollution
+    for row_idx, row_data in enumerate(first_pollution):
+        combined_row = [row_data[0]]
+        for place in places:
+            combined_row.extend(place.pollution[row_idx][1:])
+
+        data.append(combined_row)
+
+    write_csv('pollution', places, params, data)
 
 
-def main():
-    places = [
-        Place('Beijing', 2038349, 39.9042, 116.4074),
-        Place('Zhangjiakou', 2033194, 40.7675, 114.8863),
-        Place('Qinhuangdao', 1797593, 39.9354, 119.6005),
-        Place('Shijiazhuang', 1795266, 38.0428, 114.5149),
-        Place('Tianjin', 1792943, 39.3434, 117.3616),
-    ]
+def get_weather():
+    key = '4317146d63a0e039a4110e3ea201bd3d'
+    url = 'https://api.darksky.net/forecast/{}/{},{},{}'
+    params = ['pressure', 'windSpeed', 'windBearing']
 
-    hours = 10
+    delta = datetime.timedelta(hours=1)
+    data = []
 
-    pollution_params = ['co', 'no2', 'o3', 'pm10', 'pm25', 'so2']
-    weather_params = ['pressure', 'windSpeed', 'windBearing']
+    r_time = start_time
+    while r_time < end_time:
+        unix_time = int(r_time.timestamp())
 
-    headers = ['Timestamp (UTC)']
-    for place in places:
-        for param in pollution_params:
-            headers.append('{} {}'.format(place.name, param))
+        for place in places:
+            r = requests.get(url.format(key, place.lat, place.long, unix_time))
+            weather = r.json()['currently']
 
-        for param in weather_params:
-            headers.append('{} {}'.format(place.name, param))
+            hour_data = [r_time.strftime('%Y-%m-%d %H:%M')]
 
-    with open('weather.csv', 'w') as out_file:
-        csv_writer = csv.writer(out_file)
-        csv_writer.writerow(headers)
+            for param in params:
+                try:
+                    val = weather[param]
+                except KeyError:
+                    val = None
+                finally:
+                    hour_data.append(val)
 
-        s_per_hour = 3600
-        cur_time = int(time.time())
+            data.append(hour_data)
 
-        for unix_time in range(cur_time, cur_time - hours * s_per_hour, -s_per_hour):
-            get_pollution(places, unix_time, pollution_params)
-            get_weather(places, unix_time, weather_params)
-
-            row = [datetime.datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M')]
-            for place in places:
-                row.extend(place.pollution.values())
-                row.extend(place.weather.values())
-
-            csv_writer.writerow(row)
+        r_time += delta
 
 
 if __name__ == '__main__':
-    main()
+    get_pollution()
